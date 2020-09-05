07c35e72d31ac5cdea257908c1f81df76a120638
Enes Korkmaz <aeneskrkmz@gmail.com>
2020-09-06 02:36:11 +0300

    Create analys.py
    first step

---
+++ analys.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+
+sozluk = {'İsim':pd.Series(['Ada','Cem','Sibel','Ahmet','Mehmet','Ali','Veli',
+          'Ayşe','Hüseyin','Necmi','Nalan','Namık']),
+          'Meslek':pd.Series(['işçi','işçi','memur','serbest','serbest',None,None,
+          'sigortacı','işsiz',None,None,'memur']),
+          'Tarih':pd.Series(['11.11.2010','11.11.2010','11.11.2010','18.11.2011','18.11.2011',None,None,
+          None,'11.11.2010',None,'18.11.2011','18.11.2011']),          
+          'Yaş':pd.Series([21, 24, 25, 44, 31, 27, 35, 33, 42, 29, 41, 43]),
+          'ÇocukSayısı':pd.Series([None, None, None, None, None, 1, 2, 0, None, None, None, None]),
+          'Puan':pd.Series([89, 87, 77, 55, 70, 79, 73, 79, 54, 92, 61, 69])}
+
+df = pd.DataFrame(sozluk)
+
+# df.head() #ilk 5 satırı görüntüler
+# df.tail() #son 5 satırı görüntüler
+# df.sample() #rastgele 5 satırı görüntüler
+# df.shape() #satır ve sutun sayısını görüntüler
+# df.info() #veri hakkında bilgi verir
+
+df[]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
